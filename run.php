73c1f6f67995a54499266746f5096a32dc8a3c7b
Ilya Levin <bskton@gmail.com>
2018-11-03 16:37:24 +1000

    Run script was added to execute match function for all for data.php

---
+++ run.php
@@ -0,0 +1,14 @@
+<?php
+
+require 'data.php';
+require 'index.php';
+
+$n = 1;
+for($i = 0; $i < count($data); $i++) {
+  for ($j = 0; $j < count($data); $j++) {
+    if ($i != $j) {
+      $r = match($i, $j);
+      printf("%d) %d vs %d score %d:%d \n", $n++, $i, $j, $r[0], $r[1]);
+    }
+  }
+}
